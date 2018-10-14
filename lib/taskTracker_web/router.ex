5706d6a91dc10c21d4219dfe642ad488e10704f7
tina-n219 <noorani.t@husky.neu.edu>
2018-10-14 18:45:27 -0400

    database

--- lib/taskTracker_web/router.ex
+++ lib/taskTracker_web/router.ex
@@ -17,6 +17,7 @@ defmodule TaskTrackerWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/tasks", TaskController
   end
 
   # Other scopes may use custom stacks.
