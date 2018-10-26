6895b5855942112c9cea16a07216d8117e6a89fa
tina-n219 <noorani.t@husky.neu.edu>
2018-10-25 21:47:41 -0400

    edit written, needs debugging, bad input error

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,6 @@ import css from "../css/app.scss";
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 var startTime = "~N[2000-01-01 23:00:07]";
-var endTime = "~N[2000-01-01 23:00:07]";
 
 $(function () {
   let startAction = $('#start-work-button');
@@ -40,6 +39,7 @@ $(function () {
     stopAction.attr("disabled", true);
     startAction.attr("disabled", false);
     location.reload();
+
     let text = JSON.stringify({
       timeblock: {
         startTime: startTime,
@@ -60,55 +60,64 @@ $(function () {
   
   deleteAction.click((ev) => {
     console.log("delete");
-  let timeBlockID = $(ev.target).data('timeblock-id');
-  console.log({timeBlockID});
-  location.reload();
-    $.ajax("/ajax/timeblocks/" + timeBlockID, {
-      method: "delete",
-      dataType: "json",
-      contentType: "application/json; charset=UTF-8",
-    });
+    let timeBlockID = $(ev.target).data('timeblock-id');
+    console.log({timeBlockID});
+    location.reload();
+      $.ajax("/ajax/timeblocks/" + timeBlockID, {
+        method: "delete",
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+      });
   });
 
-  // let showTimePicker = ((ev) => {
-  //   let insertTime = $(ev.target).data('timeblock-id');
-  //   let blah = $('#' + insertTime)
-  //   blah.show();
-  // });
-
-  // $("#edit-work-button").click( 
-  //   showTimePicker
-  // );
-
   window.editTimeBlock = (ev) => {
-
-    // let s = $(ev.target).data('startTime-id');
-    // let e = $(ev.target).data('endTime-id');
     let timeBlock_id = $(ev).data('timeblock-id');
-    console.log(timeBlock_id);
+
     $('#startTD' + timeBlock_id).hide();
     $('#endTD' + timeBlock_id).hide();
     $('#startInput' + timeBlock_id).show();
     $('#endInput' + timeBlock_id).show();  
     
     $('#edit-work-button' + timeBlock_id).hide();
-    $('#save-work-button' + timeBlock_id).show();
+    $('#save-work-button' + timeBlock_id).show(); 
+  }
 
+  window.saveTimeBlock = (ev) => {
+    let timeBlock_id = $(ev).data('timeblock-id');
+    let task_id = $(ev).data('task-id');
 
-    // let text = JSON.stringify({
-    //   timeblock: {
-    //     startTime: startTime,
-    //     endTime: endTime,
-    //     task_id: task_id,
-    //   },
-    // });
+    let sTime = $('#start-'+timeBlock_id).val();
+    let eTime = $('#end-'+timeBlock_id).val();
 
-    // $.ajax("/ajax/timeblocks/", {
-    //   method: "post",
-    //   dataType: "json",
-    //   contentType: "application/json; charset=UTF-8",
-    //   data: text,
-    // });
+    let newStart = new Date(sTime);
+    let newEnd = new Date(eTime);
+
+    let text = JSON.stringify({
+      timeblock: {
+        startTime: newStart,
+        endTime: newEnd,
+        task_id: task_id,
+      }
+    });
+    console.log(newStart);
+    console.log(newEnd);
+
+    $.ajax("/ajax/timeblocks/" + timeBlock_id, {
+      method: "put",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: text,
+    }).done(() => {
+      location.reload();
+    });
+
+    $('#startTD' + timeBlock_id).show();
+    $('#endTD' + timeBlock_id).show();
+    $('#startInput' + timeBlock_id).hide();
+    $('#endInput' + timeBlock_id).hide();  
+    
+    $('#edit-work-button' + timeBlock_id).show();
+    $('#save-work-button' + timeBlock_id).hide();
   }
 });
 
